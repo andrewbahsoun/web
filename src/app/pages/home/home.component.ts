c1f578b56c81a3f837a466da4c0c25a2dc0a8a95
Andrew Bahsoun <bahsoun@chapman.edu>
2025-11-13 02:50:30 -0800

    updated images and feel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ElectricBorderComponent } from '../../electric-border/electric-border.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ElectricBorderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
